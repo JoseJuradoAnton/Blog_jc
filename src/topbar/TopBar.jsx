8266e0e604ab2d522f901df5705db63b3ebd6845
Jose Gonzales <pythonhack22@gmail.com>
2023-10-03 19:10:25 -0500

    update2

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,31 @@
+import './topbar.css'
+
+const TopBar = () => {
+    return (
+        <div className="top">
+            <div className="topLeft">
+                <i className="fab fa-facebook-square"></i>
+                <i className="fab fa-twitter-square"></i>
+                <i className="fab fa-pinterest-square"></i>
+                <i className="fab fa-instagram-square"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">ABOUT</li>
+                    <li className="topListItem">CONTACT</li>
+                    <li className="topListItem">WRITE</li>
+                    <li className="topListItem">LOGOUT</li>
+                </ul>
+            </div>
+            <div className="topRight">
+                <img
+                    className="topImg"
+                    src="https://images.pexels.com/photos/6195663/pexels-photo-6195663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <i className='fas fa-search'></i>
+            </div>
+        </div>
+    )
+}
+
+export default TopBar
