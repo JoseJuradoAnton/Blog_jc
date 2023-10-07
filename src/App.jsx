d4df0a466c6a3fd2b4eae3d0d198b35a93207351
Jose Gonzales <pythonhack22@gmail.com>
2023-10-07 03:57:14 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,24 @@ import Single from './pages/single/Single'
 import Write from './pages/write/Write'
 import Settings from './pages/settings/Settings'
 import Login from './pages/login/Login'
-
+import Register from './pages/register/Register'
+import {
+  BrowserRouter as router, Switch, Route,
+  Link,
+  Router
+} from 'react-router-dom'
 function App() {
 
 
   return (
     <>
+
       <div className='App'>
         <Topbar />
-        <Login />
+        <Register />
       </div>
+
+
     </>
   )
 }
