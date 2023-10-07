1486812b31283fb4ec703b115d4cb68997102cf2
Jose Gonzales <pythonhack22@gmail.com>
2023-10-07 18:51:10 -0500

    update2

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className='header'>
             <div className="headerTitles">
-                <span className='headerTitleSm' >React & Node</span>
+                <span className='headerTitleSm' >Jose Gonzales Developer</span>
                 <span className='headerTitleLm'>Blog</span>
             </div>
             <img className='headerImg' src="https://images.pexels.com/photos/5935794/pexels-photo-5935794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
