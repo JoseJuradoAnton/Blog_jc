46669d076181d06c2387a6c93d7b056b198f3b88
Jose Gonzales <pythonhack22@gmail.com>
2023-10-09 21:46:41 -0500

    update1

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -1,6 +1,8 @@
 import './topbar.css'
+import { Link } from "react-router-dom";
 
 const TopBar = () => {
+    const user = false;
     return (
         <div className="top">
             <div className="topLeft">
@@ -11,17 +13,45 @@ const TopBar = () => {
             </div>
             <div className="topCenter">
                 <ul className="topList">
-                    <li className="topListItem">HOME</li>
-                    <li className="topListItem">ABOUT</li>
-                    <li className="topListItem">CONTACT</li>
-                    <li className="topListItem">WRITE</li>
-                    <li className="topListItem">LOGOUT</li>
+                    <li className="topListItem">
+                        <Link className='link' to={"/"}>HOME</Link>
+                    </li>
+                    <li className="topListItem">
+                        <Link className='link' to={"/"}>ABOUT</Link>
+                    </li>
+                    <li className="topListItem">
+                        <Link className='link' to={"/"}>CONTACT</Link>
+                    </li>
+                    <li className="topListItem">
+                        <Link className='link' to={"/"}>WRITE</Link>
+                    </li>
+                    <li className="topListItem">
+                        {user && "LOGOUT"}
+                    </li>
                 </ul>
             </div>
             <div className="topRight">
-                <img
-                    className="topImg"
-                    src="https://images.pexels.com/photos/6195663/pexels-photo-6195663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                {
+                    user ? (
+
+                        <img
+                            className="topImg"
+                            src="https://images.pexels.com/photos/6195663/pexels-photo-6195663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    ) : (
+
+                        <ul className='topList'>
+                            <li className='topListItem'>
+
+                                <Link className='link' to="/login">LOGIN</Link>
+                            </li>
+
+                            <li className='topListItem'>
+                                <Link className='link' to="/register">REGISTER</Link>
+                            </li>
+                        </ul>
+                    )
+
+                }
                 <i className='searchIcon fas fa-search'></i>
             </div>
         </div>
